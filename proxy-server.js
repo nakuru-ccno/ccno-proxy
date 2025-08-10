af2360b7d39fdd672fe8e727b0be40ce2e77d41a
nakuru-ccno <nakurucountychiefnursingoffice@gmail.com>
2025-08-10 17:24:01 +0200

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 
 const app = express();
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwnZS-ciGtFTXcwbSw1VY3doeBSkX30XDGVEeDQsjNlfy9jx0Paxn_m2nxWLyMq4AORw/exec';
+// UPDATED Apps Script Web App URL
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxSh9usourM0Fpy40ZYqTaN8_X6YmLX4AgpuRzeYihxNB3YpCAfcIkWh1tBCNVlAo1bxw/exec';
 
 // Use CORS middleware to allow requests only from your frontend origin
 app.use(cors({
@@ -42,4 +43,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Proxy server running on port ${PORT}`);
 });
-
